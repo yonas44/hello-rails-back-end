32eefbcfefde4d17223c30394d34b6cc3baf237a
yonas44 <tesfu.yonas1350@gmail.com>
2023-03-28 16:40:25 +0300

    Add model and controller for handling requests, and setup routes.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+message1 = Message.create(text: 'Bonjour! ')
+message2 = Message.create(text: 'Hola!')
+message3 = Message.create(text: 'Konnichiwa!')
+message4 = Message.create(text: 'Zdraveyte!')
+message5 = Message.create(text: 'Hallo!')
